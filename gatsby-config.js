873c2a5520fe20d867edd3c755b0c5ca61f22dee
livprojects <liv.audigane@gmail.com>
2021-08-13 23:07:06 +0200

    pages creation + helmet for meta tags

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,5 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "Tech Teapot",
   },
-  plugins: ["gatsby-plugin-gatsby-cloud"],
+  plugins: ["gatsby-plugin-gatsby-cloud", "gatsby-plugin-react-helmet"],
 };
