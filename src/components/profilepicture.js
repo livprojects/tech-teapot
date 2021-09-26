59c621ac1bb18b3d02443903f33c2882bc1bcece
livprojects <liv.audigane@gmail.com>
2021-09-26 19:14:17 +0200

    ajout photo de profil

---
+++ src/components/profilepicture.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import '../styles/style.scss';
+import pic from '../images/IMG_8882.png';
+
+const ProfilePic = () => {
+return (
+    <div className='gridbox-profilepic'>WHITE SPACE
+    WHITE SPACE
+    WHITE SPACE
+    WHITE SPACE
+        {/* <img className='gridbox-profilepic-img'src={pic} alt='Liv Audigane'></img> */}
+    </div>
+)
+}
+
+export default ProfilePic;
