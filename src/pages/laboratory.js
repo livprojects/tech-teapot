db8b8128023dbb287d801fc59224e787bbbd313c
livprojects <liv.audigane@gmail.com>
2021-09-26 19:14:57 +0200

    ajout lorem ipsum

---
+++ src/pages/laboratory.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import "../styles/style.scss";
+
+const Laboratory = () => {
+
+    return (
+    <div className="gridbox-lab">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec imperdiet dignissim tristique. Fusce iaculis neque non convallis imperdiet. Integer libero est, bibendum sit amet varius eu, tincidunt nec nunc. Proin nunc lectus, lacinia ut semper eu, placerat vel justo. Nullam ac lectus sed lacus pulvinar vestibulum et et dolor. Quisque in tincidunt eros. Proin ligula lacus, condimentum in varius quis, pellentesque eu enim. Etiam ornare tempor orci sit amet bibendum. Aenean eget tortor metus. Ut a pretium leo.</div>
+    ) 
+
+}
+
+export default Laboratory;
