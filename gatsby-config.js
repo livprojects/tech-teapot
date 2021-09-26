167a2ad247f08c9fe3e40216950ab81656509b90
livprojects <liv.audigane@gmail.com>
2021-09-26 19:13:44 +0200

    maj gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,9 @@
+const path = require(`path`);
+
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "Tech Teapot",
+    siteUrl: 'https://www.yourdomain.tld',
+    title: 'Tech Teapot',
   },
-  plugins: ["gatsby-plugin-gatsby-cloud", "gatsby-plugin-react-helmet", "gatsby-plugin-sass"],
+  plugins: ['gatsby-plugin-gatsby-cloud', 'gatsby-plugin-react-helmet', 'gatsby-plugin-sass'],
 };
