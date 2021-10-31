59a8907d89a3611a83d243b837032ade18a3f8f5
livprojects <liv.audigane@gmail.com>
2021-10-31 18:14:05 +0100

    translation json cleanup

--- src/i18n.js
+++ src/i18n.js
@@ -23,7 +23,7 @@ i18n
     resources,
     lng: "en",
 
-    keySeparator: false, // we do not use keys in form messages.welcome
+    keySeparator: ".", // we do not use keys in form messages.welcome
 
     interpolation: {
       escapeValue: false // react already safes from xss
