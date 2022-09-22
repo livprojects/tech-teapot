a04084989e47d75e2586735375da8fc54725c5cc
livprojects <liv.audigane@gmail.com>
2022-09-22 22:28:36 +0200

    sliding panel class name

--- src/components/tools/shortcuts/shortcuts.js
+++ src/components/tools/shortcuts/shortcuts.js
@@ -1,14 +1,26 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import './shortcuts.css';
 
 const Shortcuts = () => {
+
+    const [showPanel, setShowPanel] = useState(false);
+
+    const togglePanel = () => {
+        setShowPanel(!showPanel);
+    }
+
 return (
-    <nav className='gridbox-shortcuts-nav'>
-        <ul className='gridbox-shortcuts-nav-ul'>
-            <li>CV</li>
-            <li>Contact</li>
-            <li>Github</li>
-        </ul>
+    <nav className={`gridbox-shortcuts-nav ${showPanel ? "show" : ""}`} onClick={() => togglePanel()}>
+        <div>
+            <button className='gridbox-shortcuts-nav-button'>More links</button>
+        </div>
+        <div>
+            <ul className='gridbox-shortcuts-nav-ul'>
+                <li>CV</li>
+                <li>Contact</li>
+                <li>Github</li>
+            </ul>
+        </div>
     </nav>
 )
 }
