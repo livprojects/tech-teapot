c56fcdf510f056a3de7065b3e9a2c737776d9c47
livprojects <liv.audigane@gmail.com>
2023-01-02 00:08:48 +0100

    projetcs data update

--- src/components/home/lifeline/lifeline.js
+++ src/components/home/lifeline/lifeline.js
@@ -1,12 +1,15 @@
 import * as React from 'react';
 import './lifeline.css';
+import cv from '../../../data/CV_Janvier_2023.pdf';
 
 const Lifeline = () => {
+
+    const cvPath = `${cv}+'#toolbar=0'`
 
     return (
         <div className="gridbox-center gridbox-lifeline">
             <div className="cv-wrapper">
-                <iframe src="../../data/CV_Janvier_2023.pdf"></iframe>
+                <iframe id="cv" title="cv" src={cvPath}  width="100%" height="100%"></iframe>
             </div>
         </div>
     ) 
