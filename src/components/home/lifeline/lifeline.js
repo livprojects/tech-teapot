33f6eacc91af2ef021ccf1469a7dde6ab675c1ef
livprojects <liv.audigane@gmail.com>
2023-01-02 00:15:27 +0100

    update cv path

--- src/components/home/lifeline/lifeline.js
+++ src/components/home/lifeline/lifeline.js
@@ -4,12 +4,10 @@ import cv from '../../../data/CV_Janvier_2023.pdf';
 
 const Lifeline = () => {
 
-    const cvPath = `${cv}+'#toolbar=0'`
-
     return (
         <div className="gridbox-center gridbox-lifeline">
             <div className="cv-wrapper">
-                <iframe id="cv" title="cv" src={cvPath}  width="100%" height="100%"></iframe>
+                <iframe id="cv" title="cv" src={cv}  width="100%" height="100%"></iframe>
             </div>
         </div>
     ) 
