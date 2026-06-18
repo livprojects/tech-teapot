75c2c544588eb50b81e19cb348f82b2d6b0ae7c9
Liv Audigane <laudigane@lexpress.fr>
2026-06-18 14:45:15 +0200

    resume update and padding fix

--- src/components/tools/navigation/navigation.jsx
+++ src/components/tools/navigation/navigation.jsx
@@ -13,6 +13,7 @@ const Navigation = ({t, activeComponent}) => {
     const upArchive = t('navigation.archives-title').toUpperCase();
     const upHome = t('navigation.home-title').toUpperCase();
     const upLifeLine = t('navigation.lifeline-title').toUpperCase();
+    const cvPath = t('navigation.cv-path');
 
 
 
@@ -22,7 +23,7 @@ const Navigation = ({t, activeComponent}) => {
                 <Link to="/" className={`thumbnail ${activeComponent ==='/' ? "active" : "inactive"}`}>{upHome}</Link>
                 <Link to="/programming" className={`thumbnail ${activeComponent ==='/programming' ? "active" : "inactive"}`}>{upLab}</Link>
                 <Link to="/journalism" className={`thumbnail ${activeComponent ==='/journalism' ? "active" : "inactive"}`}>{upArchive}</Link>
-                <Link to="/lifeline" className={`thumbnail ${activeComponent ==='/lifeline' ? "active" : "inactive"}`}>{upLifeLine}</Link>
+                <a href={cvPath} target="_blank" rel="noopener noreferrer" className="thumbnail inactive">{upLifeLine}</a>
             </div>
             <Shortcuts t={t}></Shortcuts>
 
